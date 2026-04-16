0fbde85a682ded4b98342fa56f1de1c8eadb3722
k.boenko <k.boenko@bftcom.com>
2026-04-16 11:57:53 +0700

    add pageable history logic

--- src/main/java/com/artix/test/bonus/repository/BonusHistoryRepository.java
+++ src/main/java/com/artix/test/bonus/repository/BonusHistoryRepository.java
@@ -1,11 +1,15 @@
 package com.artix.test.bonus.repository;
 
 import com.artix.test.bonus.model.BonusHistory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BonusHistoryRepository extends PagingAndSortingRepository<BonusHistory, Long> {
 
-
+    @Query("SELECT bh.id, bh.amount, bh.operationDate from BonusHistory bh JOIN bh.card c where c.number like :number")
+    Page<BonusHistory> getByCardNumber(String number, Pageable pageable);
 }
--- src/main/java/com/artix/test/bonus/service/impl/BonusHistoryServiceImpl.java
+++ src/main/java/com/artix/test/bonus/service/impl/BonusHistoryServiceImpl.java
@@ -1,19 +1,22 @@
 package com.artix.test.bonus.service.impl;
 
 import com.artix.test.bonus.model.BonusHistory;
+import com.artix.test.bonus.repository.BonusHistoryRepository;
 import com.artix.test.bonus.service.BonusHistoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
+@RequiredArgsConstructor
 public class BonusHistoryServiceImpl implements BonusHistoryService {
+
+    private final BonusHistoryRepository bonusHistoryRepository;
 
     @Override
     public Page<BonusHistory> getHistoryByCardNumber(String cardNumber, Pageable pageable) {
 
-        return null;
+        return bonusHistoryRepository.getByCardNumber(cardNumber, pageable);
     }
 }
